357b57941e36a0d549dcb0d3a21cfbdfd47ff27f
mateus <mateusreis@gmail.com>
2025-03-31 18:59:44 -0300

    refactor

--- single_product.php
+++ single_product.php
@@ -99,6 +99,10 @@
 
 } 
 
+
+
+
+
 /**
  * Salva os valores dos campos personalizados do produto.
  *
@@ -371,23 +375,79 @@ add_shortcode('siteoficial', 'show_siteoficial'); // Cria um shortcode para exib
 
 
 
-function format_title_hook() {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+function remove_stock_availability_hook() {
+    $priority = has_action('woocommerce_single_product_summary', 'woocommerce_template_single_stock');
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_stock', $priority);
+}
+add_action('plugins_loaded', 'remove_stock_availability_hook');
+
+
+
+
+
+function remove_me() {
+    $priority = has_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta');
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', $priority);
+}
+add_action('plugins_loaded', 'remove_me');
+
+
+
+
+// function remove_me2() {
+//     $priority = has_action('woocommerce_template_single_price', 'woocommerce_template_single_meta');
+//     remove_action('woocommerce_template_single_price', 'woocommerce_template_single_meta', $priority);
+// }
+// add_action('plugins_loaded', 'remove_me2');
+
+
+
+
+// Remove SKU and Categories from single product page
+// remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+
+// remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+// remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
+// remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+// remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+// remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+// remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50 );
+
+
+
+function format_title_hook() {
     $priority = has_action('woocommerce_single_product_summary', 'woocommerce_template_single_title');
     remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', $priority);
-
-    add_action( 'woocommerce_single_product_summary', 'show_titulo', 5);
-    
-    
-    // echo "<hr/>result of add_result = " . $add_result . "<hr/>";
-    // echo "<hr/>result of remove_result = " . $remove_result . "<hr/>";
+    add_action( 'woocommerce_single_product_summary', 'show_product_title', 5);
 }
-
-
-function show_titulo() {
-  echo '<h3 class="woocommerce-loop-product_title">cccwsss ' . get_the_title() . '</h3>';
+function show_product_title() {
+  echo '<h3 class="woocommerce-loop-product_title">' . get_the_title() . '</h3>';
 }
-
 add_action('plugins_loaded','format_title_hook');
 
 
@@ -402,16 +462,20 @@ function format_prices_hook() {
 }
 
 function show_precos(){
-  echo '<h3 class="woocommerce-loop-product_title">xxxffsss NOVOS PRECOS</h3>';
+//   echo '<h3 class="woocommerce-loop-product_title">xxxffsss NOVOS PRECOS</h3>';
   echo '<div class="pricing-section">
+
+
+
             <div class="main-price">
                 <span class="price">R$ 6.852,31</span>
                 <span class="payment-term">à vista</span>
             </div>
             <p class="savings">Economize: R$ 761,37</p>
-            
             <p class="regular-price">R$ 7.613,68</p>
             
+
+
             <div class="installment-info">
                 <div class="card-icon">
                     <i class="far fa-credit-card"></i>
@@ -420,9 +484,11 @@ function show_precos(){
                     <span>até <strong>10x</strong> de <strong>R$ 761,36</strong> sem juros</span>
                     <a href="#" class="payment-options">mais formas de pagamento</a>
                 </div>
-            </div>
+            </div>'; 
             
-            <div class="purchase-section">
+            // custom_wc_template_single_price();
+
+            echo '<div class="purchase-section">
                 <div class="quantity-selector">
                     <input type="text" value="1" class="quantity-input">
                     <div class="quantity-buttons">
@@ -438,242 +504,146 @@ function show_precos(){
             
             <button class="whatsapp-button">
                 <i class="fab fa-whatsapp"></i> Comprar pelo whatsapp
-            </button>
-            
-            <p class="availability">Disponibilidade: <span>Disponível</span></p>
-        </div>';
+            </button>';
+
+            display_product_availability();
+            show_stock_availability();
+            echo '</div>';
+
 }
 
 add_action('plugins_loaded','format_prices_hook');
 
 
 
-// add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
-// add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
-// add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
-// add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
-// add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
-// add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 5
+
+function display_product_availability() {
+    global $product;
+    
+    if (!$product) {
+        return;
+    }
+
+    $availability = $product->get_availability();
+    $stock_status = $product->get_stock_status();
+
+    if ($stock_status === 'instock') {
+        echo '<p class="availability">Disponibilidade: <span>Disponível</span></p>';
+    } elseif ($stock_status === 'outofstock') {
+        echo '<p class="availability">Disponibilidade: <span class="out-of-stock">Indisponível</span></p>';
+    } elseif ($stock_status === 'onbackorder') {
+        echo '<p class="availability">Disponibilidade: <span class="backorder">Sob Encomenda</span></p>';
+    }
+}
 
 
+function show_stock_availability() {
+    global $post;
+    
+    // Get the product object
+    $product = wc_get_product($post->ID);
+    
+    if (!$product) {
+        return;
+    }
+
+    // Get stock status and quantity
+    $stock_status = $product->get_stock_status();
+    $stock_quantity = $product->get_stock_quantity();
+
+    echo '<div class="product-custom-field">';
+    echo '<span class="wp-block-heading stock-status">';
+    
+    switch($stock_status) {
+        case 'instock':
+            if ($stock_quantity) {
+                echo '<strong>Em estoque: ' . $stock_quantity . ' unidades</strong>';
+            } else {
+                echo '<strong>Em estoque</strong>';
+            }
+            break;
+            
+        case 'outofstock':
+            echo '<strong>Fora de estoque</strong>';
+            break;
+            
+        case 'onbackorder':
+            echo '<strong>Disponível por encomenda</strong>';
+            break;
+            
+        default:
+            echo '<strong>Status de estoque não disponível</strong>';
+    }
+    
+    echo '</span>';
+    echo '</div>';
+}
 
 
-add_filter( 'woocommerce_get_price_html', 'custom_price_html', 1000, 2 );
-function custom_price_html( $price, $product ) {
-  if ( $product->is_type( 'variable' ) ) {
-      // Handle variable product prices.
-      $variations = $product->get_available_variations();
-      if ( ! empty( $variations ) ) {
-          $min_price = $max_price = null;
-          foreach ( $variations as $variation ) {
-              $variation_product = wc_get_product( $variation['variation_id'] );
-              $regular_price = $variation_product->get_regular_price();
-              $sale_price = $variation_product->get_sale_price();
+  /**
+   * Reformata a exibição de preços para produtos variáveis.
+   *
+   * Esta função remove a exibição padrão de preços de produtos variáveis
+   * e a substitui por uma exibição personalizada, listando os preços
+   * para cada variação do produto.
+   */
+//   add_action('woocommerce_before_single_product_summary', 'dd');
+  function dd()
+  {
+      if (is_product()) {
 
-              if ( $sale_price !== '' && $sale_price < $regular_price ) {
-                  $current_price = $sale_price;
-              } else {
-                  $current_price = $regular_price;
-              }
+          global $post;
+          $product = wc_get_product($post->ID);
 
-              if ( $min_price === null || $current_price < $min_price ) {
-                  $min_price = $current_price;
-              }
-              if ( $max_price === null || $current_price > $max_price ) {
-                  $max_price = $current_price;
-              }
-          }
-          if ( $min_price === $max_price ) {
-              $price = wc_price( $min_price ) . ' <span class="custom-var-price-text">Only</span>';
+          if ($product->is_type('variable')) {
+              // Remove a exibição padrão do preço do produto variável.
+              remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price');
+
+              // Adiciona a função customizada para exibir os preços.
+              add_action('woocommerce_single_product_summary', 'custom_wc_template_single_price', 25);
+             
           } else {
-              // $price = wc_price( $min_price ) . ' - ' . wc_price( $max_price ) . ' <span class="custom-var-price-text">Range</span>';
-              $price = "<div class='variation-box'>";
-              $price .= "<span class='price_sale_variable'>A partir de <span class='price'>" . wc_price($min_price) . '</span></span>'; 
-              $price .= "</div>";              
-          }
-      }
-  } else {
-      // Handle simple product prices.
-      if ( $product->is_on_sale() ) {
-
-          // echo "is_on_sale";
-          $regular_price = $product->get_regular_price();
-          $sale_price = $product->get_sale_price();
-          $price = '<span class="price_sale">De <del>' . wc_price( $regular_price ) . '</del> por <span class="price"><ins>' . wc_price( $sale_price ) . '</ins> à vista</span></span>';
-      } else {
-                      //   remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price');
-          //   add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 25);
-          // $price = $price . ' <span class="custom-simple-price-text"> Em até PARCELSx</span>';
-      }
-  }
-
-  return $price;
-}
-
-
-function custom_variable_stock_style() {
-  global $product;
-
-  if ($product && $product->is_type('variable')) {
-      // Get available variations
-      $variations = $product->get_available_variations();
-
-      foreach ($variations as $variation) {
-          $variation_id = $variation['variation_id'];
-          $variation_product = wc_get_product($variation_id);
-
-          if ($variation_product && $variation_product->is_in_stock()) {
-              // If in stock, inject inline style to the variation's stock status.
-              add_filter('woocommerce_variation_option_name', function ($term_name, $variation, $attribute_name, $variation_product) use ($variation_id) {
-
-                  if($variation_product->get_id() === $variation_id){
-
-                      $stock_quantity = $variation_product->get_stock_quantity();
-
-                      if($stock_quantity > 0){
-                          $term_name .= ' <span style="color:red;">' . __('In Stock', 'woocommerce') . ' (' . $stock_quantity . ')</span>';
-                      } else {
-                          $term_name .= ' <span style="color:red;">' . __('In Stock', 'woocommerce') . '</span>';
-                      }
-
-                  }
-
-                  return $term_name;
-              }, 999, 4);
-
+              // Se o produto não for variável, restaura a exibição padrão do preço.
+              remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price');
+              add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 25);
           }
       }
   }
-}
-add_action('woocommerce_before_variations_form', 'custom_variable_stock_style');
 
-//If you want to change the style of the stock quantity displayed on the single variation page after a variation is selected:
+  function custom_wc_template_single_price()
+  {
+      global $product;
+      $html = '';
+      // Executa apenas para produtos variáveis.
+      if ($product->is_type('variable')) {
 
-function custom_variation_stock_html($html, $variation) {
+          // Obtém as variações disponíveis do produto.
+          $variations = $product->get_available_variations();
 
-  if($variation && $variation->is_in_stock()){
-      $stock_quantity = $variation->get_stock_quantity();
+          // Itera sobre as variações para exibir os preços.
+          foreach ($variations as $variation) {
 
-      if($stock_quantity > 0){
-          $html = '<p class="stock in-stock" style="color:red;">' . sprintf( __( 'Availability: %s in stock', 'woocommerce' ), wc_format_stock_quantity( $stock_quantity ) ) . '</p>';
-      } else {
-          $html = '<p class="stock in-stock" style="color:red;">' . __( 'Availability: In stock', 'woocommerce' ) . '</p>';
+              // Obtém os preços normal e promocional da variação.
+              $product_regular_price = $variation['display_regular_price'];
+              $product_price = $variation['display_price'];
+
+              // Obtém o título da variação (ex: cor).
+              $variation_title = $variation['attributes']['attribute_cores'];
+
+              // Monta o HTML para exibir o preço da variação.
+              $html = "<span class='variation-title'>" . $variation_title . "</span>";
+              $html .= "<p class='price'>";
+              $html .= "<span class='woocommerce-Price-amount amount'>R$ " . number_format($product_regular_price, 2, ',', '.') . "</span> em 10x </br>";
+              $html .= "<span class='woocommerce-Price-amount amount'>R$ " . number_format($product_price, 2, ',', '.') . "</span> à vista";
+              $html .= "</p>";
+
+              // Exibe o HTML do preço da variação.
+              echo "<div class='variation-box'>" . $html . "</div>";
+          }
+
+          return $html;
       }
-
   }
-
-  return $html;
-}
-
-add_filter('woocommerce_variation_availability_html', 'custom_variation_stock_html', 999, 2);
-
-
-
-// formata titulo no loop
-// remove_action( 'woocommerce_shop_loop_item_title','woocommerce_template_loop_product_title', 1 );
-// add_action('woocommerce_shop_loop_item_title', 'abChangeProductsTitle', 10 );
-// function abChangeProductsTitle() {
-//     echo '<h4 class="woocommerce-loop-product_title">xxccxxccxxxx<a href="'.get_the_permalink().'">' . get_the_title() . '</a></h4>';
-// }
-
-// sem tem template, nao funciona?
-// remove_action( 'woocommerce_template_single_title','woocommerce_template_loop_product_title', 1111111 );
-// add_action('woocommerce_template_single_title', 'chagnesingletiotle', 1111111 );
-// function chagnesingletiotle() {
-//     echo '<h2 class="woocommerce-loop-product_title">xoopoop<a href="'.get_the_permalink().'">' . get_the_title() . '</a></h2>';
-// }
-
-
-/*
-
-add_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
-add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
-add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
-add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
-add_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
-
-add_action( 'woocommerce_before_subcategory', 'woocommerce_template_loop_category_link_open', 10 );
-add_action( 'woocommerce_shop_loop_subcategory_title', 'woocommerce_template_loop_category_title', 10 );
-add_action( 'woocommerce_after_subcategory', 'woocommerce_template_loop_category_link_close', 10 );
-
-add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
-add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
-
-*/
-
-
-
-
-  // =================================================================================================
-  // =================================================================================================
-  // =================================================================================================
-  
- 
-
-
-
-//   /**
-//    * Reformata a exibição de preços para produtos variáveis.
-//    *
-//    * Esta função remove a exibição padrão de preços de produtos variáveis
-//    * e a substitui por uma exibição personalizada, listando os preços
-//    * para cada variação do produto.
-//    */
-//   add_action('woocommerce_before_single_product_summary', 'check_if_variable_first');
-//   function check_if_variable_first()
-//   {
-//       if (is_product()) {
-
-//           global $post;
-//           $product = wc_get_product($post->ID);
-
-//           if ($product->is_type('variable')) {
-//               // Remove a exibição padrão do preço do produto variável.
-//               remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price');
-
-//               // Adiciona a função customizada para exibir os preços.
-//               add_action('woocommerce_single_product_summary', 'custom_wc_template_single_price', 25);
-//               function custom_wc_template_single_price()
-//               {
-//                   global $product;
-
-//                   // Executa apenas para produtos variáveis.
-//                   if ($product->is_type('variable')) {
-
-//                       // Obtém as variações disponíveis do produto.
-//                       $variations = $product->get_available_variations();
-
-//                       // Itera sobre as variações para exibir os preços.
-//                       foreach ($variations as $variation) {
-
-//                           // Obtém os preços normal e promocional da variação.
-//                           $product_regular_price = $variation['display_regular_price'];
-//                           $product_price = $variation['display_price'];
-
-//                           // Obtém o título da variação (ex: cor).
-//                           $variation_title = $variation['attributes']['attribute_cores'];
-
-//                           // Monta o HTML para exibir o preço da variação.
-//                           $html = "<span class='variation-title'>" . $variation_title . "</span>";
-//                           $html .= "<p class='price'>";
-//                           $html .= "<span class='woocommerce-Price-amount amount'>R$ " . number_format($product_regular_price, 2, ',', '.') . "</span> em 10x </br>";
-//                           $html .= "<span class='woocommerce-Price-amount amount'>R$ " . number_format($product_price, 2, ',', '.') . "</span> à vista";
-//                           $html .= "</p>";
-
-//                           // Exibe o HTML do preço da variação.
-//                           echo "<div class='variation-box'>" . $html . "</div>";
-//                       }
-//                   }
-//               }
-//           } else {
-//               // Se o produto não for variável, restaura a exibição padrão do preço.
-//               remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price');
-//               add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 25);
-//           }
-//       }
-//   }
-
-
 
 // regular and sales prices, codigo do roberto
 
